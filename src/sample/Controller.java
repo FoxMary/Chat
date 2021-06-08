edaf86786a33161813ce3148e231b878e752fb6f
Fox <Fox.mary@rambler.ru>
2021-06-09 01:53:03 +0300

    дз 4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextArea textArea;
+    @FXML
+    TextField textField;
+
+    public void textFieldEnter(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+    }
+
+    public void clientBtnClick(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
